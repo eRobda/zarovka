8f0454402b5d33b703651b328359d321d7ac5f8d
eRobda <51453874+eRobda@users.noreply.github.com>
2022-06-02 09:22:58 +0200

    Add files via upload

---
+++ proVĚRKA/proVĚRKA/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proVĚRKA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zarovka zarovka = new Zarovka(false,35,0);
+
+            //rozsveceni a zhasinani
+            zarovka.Rozsvit();
+            Console.WriteLine("sviti? " + zarovka.Sviti);
+            zarovka.Zhasni(); 
+            Console.WriteLine("sviti? " + zarovka.Sviti);
+            zarovka.Rozsvit();
+            Console.WriteLine("sviti? " + zarovka.Sviti);
+            zarovka.Zhasni();
+            Console.WriteLine("sviti? " + zarovka.Sviti);
+
+            //vypsani prikonu
+            Console.WriteLine("Prikon je: " + zarovka.Prikon);
+
+            //vypsani poctu rozsviceni
+            Console.WriteLine("Pocet rozsviceni je: " + zarovka.DejPocetRozsviceni());
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ proVĚRKA/proVĚRKA/Zarovka.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proVĚRKA
+{
+    public class Zarovka
+    {
+        private static bool sviti;
+        private int prikon;
+        private static int pocetRozsviceni;
+
+        public Zarovka()
+        {
+
+        }
+
+        public Zarovka(bool sviti, int prikon, int pocetRozsviceni)
+        {
+
+        }
+
+        //vlastnosti
+        public bool Sviti
+        {
+            get { return sviti; }
+        }
+        public int Prikon
+        {
+            get { return this.prikon; }
+        }
+
+        public void Rozsvit()
+        {
+            sviti = true;
+            pocetRozsviceni += 1;
+        }
+        public void Zhasni()
+        {
+            sviti = false;
+        }
+        public int DejPocetRozsviceni()
+        {
+            return pocetRozsviceni;
+        }
+    }
+}
